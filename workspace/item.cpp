af0d95ba5a9f506930749d97c145031c269e4a59
fredoo <fred.fern91@gmail.com>
2015-04-23 18:34:42 +0200

    suppression de 2 ,3 print pour le débug

--- workspace/item.cpp
+++ workspace/item.cpp
@@ -29,7 +29,7 @@ Item::Item(QString name): QListWidgetItem(), QGraphicsScene()
 
 }
 
-Item::Item(Plat plat,int imgSizeW,int imgSizeH ,QColor background)
+Item::Item(Plat plat,int imgSizeW,int imgSizeH ,QColor background): QListWidgetItem(), QGraphicsScene()
 {
     qDebug()<<imgSizeW;
     this->imgSizeW= imgSizeW;
