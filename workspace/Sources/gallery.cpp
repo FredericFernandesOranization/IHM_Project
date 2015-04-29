e1ac1d1109e11894690216d0b7527ca86d6d82cc
MattiaPrimavera <sconqua@gmail.com>
2015-04-29 15:27:19 +0200

    Fonction showType, classe Galerie

--- workspace/Sources/gallery.cpp
+++ workspace/Sources/gallery.cpp
@@ -11,27 +11,24 @@ Gallery::Gallery() : QWidget()
     //Gallery Layout
     this->layout = new FlowLayout;
     this->setLayout(this->layout);
-    this->layout->setSpacing(20
-
-                             );
+    this->layout->setSpacing(20);
 
     //loading Database
     this->database = Database::getInstance();
 
     //Adding Gallery Items
-    for(int i=0; i<this->database->getNbDishes(); i++){
+    /*for(int i=0; i<this->database->getNbDishes(); i++){
         //QWidget* q = new QWidget;
         Item *item = new Item(this->database->getDish(i));
         this->layout->addWidget(item);
-    }
-    //this->showType(QString("boisson"));
+    }*/
+    this->showType(QString("boisson"));
 
 }
 
 void Gallery::showType(QString type)
 {
     QList<Plat*> listePlats = this->database->filterType(type);
-    this->layout = new FlowLayout;
     for(int i=0; i<listePlats.size(); i++){
         Item *item = new Item(*listePlats.at(i));
         this->layout->addWidget(item);
