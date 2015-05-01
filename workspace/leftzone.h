5fa3fda578c4010cf077a2ce96ab4ac738449c9d
MattiaPrimavera <sconqua@gmail.com>
2015-05-02 00:26:05 +0200

    conflict solved

--- workspace/leftzone.h
+++ workspace/leftzone.h
@@ -12,23 +12,15 @@
 
 class LeftZone : public QWidget
 {
+    Q_OBJECT
 public:
     LeftZone();
 
 private slots:
     void getUnclicked(QString label);
-<<<<<<< HEAD
-<<<<<<< HEAD
 private:
     QList<MenuButton*> buttonLists;
-=======
-    void test();
->>>>>>> 3e7768cfc11cda64c20e2f1083b52d5fc27da223
-||||||| merged common ancestors
-    void test();
-=======
 
->>>>>>> 8757284eaeac6c2bdd6be628d8b85bb7a1dbc2e2
 };
 
 #endif // LEFTZONE_H
